c43fc843003a76e78491ea95c4d216da2e94722d
AKclever <aleksiskrass@gmail.com>
2025-02-21 15:25:14 +0200

    initial Spring boot backend setup

---
+++ src/test/java/com/example/goodnight_backend/GoodnightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodnight_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodnightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
